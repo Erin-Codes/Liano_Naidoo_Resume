8491975900d9bf84c428b5406799f1d8d938e780
[Liano Naidoo] <[lianonaidoo2001@gmail.com]>
2020-05-15 16:11:03 +0200

    Added user input, and prints it.

--- helloWorld.java
+++ helloWorld.java
@@ -3,7 +3,11 @@ public class helloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner.out.println("Git is Awesome!")
+		System.out.println("Git is Awesome!");
+		System.out.println("Enter your name:\n ");
+		Scanner input = new Scanner(System.in);
+		String name = input.nextLine();
+		System.out.println("Your name is "+name);
 	}
 
 }
